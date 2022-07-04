38ceb17d15ec6a53ee3e3846f0f0b5c1d27d82ee
Wilgot <wilgotwardell@hotmail.com>
2022-07-04 21:39:18 +0200

    virtual grid

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ canvas.height = 750;
 let lines = [];
 let lineAmount = 35;
 let lineGap = canvas.width/lineAmount;
+
+let squares = [];
+
+let gridX = [];
+let gridY = [];
+
 class line {
     constructor(x, y, endX, endY){
         this.x = x;
@@ -26,7 +32,6 @@ class line {
 
 class grid{
     constructor(){
-        this.x = 0;
     }
     draw(){
         
@@ -39,15 +44,28 @@ class grid{
         for(let i = 0; i < canvas.width/lineGap; i++){
             //vertical lines
             lines.push(new line(i*lineGap, 0, i*lineGap, canvas.height));
-            for(let z = i; z < canvas.height/lineGap; z++){
-                //horizontal lines
-                lines.push(new line(0, z*lineGap, canvas.width, z*lineGap));
-            }
         }
+        for(let z = 0; z < canvas.height/lineGap; z++){
+            //horizontal lines
+            lines.push(new line(0, z*lineGap, canvas.width, z*lineGap));
+        }
+
         for(let i = 0; i < lines.length; i++){lines[i].draw();}
     }
 }
 let grid1 = new grid();
+
+class fillSquare{
+    constructor(x, y, size){
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+    draw(){
+        c.fillStyle = 'green';
+        c.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
 
 const mouse = {
     x: undefined,
@@ -57,25 +75,37 @@ window.addEventListener('click', function(event) {
     let rect = canvas.getBoundingClientRect();
     mouse.x = event.x - Math.floor(rect.left);
     mouse.y = event.y - Math.floor(rect.top);
-    
+    if(mouse.x < 0 || mouse.x > canvas.width){
+        mouse.x = undefined;
+    }
+    if(mouse.y < 0 || mouse.y > canvas.height){
+        mouse.y = undefined;
+    }
+    squares.push(new fillSquare(mouse.x,mouse.y, lineGap))
     console.log(mouse.x + " " + mouse.y);
 });
 
-function setup(){
-    
-}
-setup();
+
 
 function update(){
-    refrech();
-    grid1.draw();
-    
+    for(i = 0; i < squares.length; i++){
+        squares[i].draw();
+    }
     requestAnimationFrame(update);
 }
 update();
 
-function refrech(){
-    for(let i = 0; i < lines.length; i++){lines.splice(i, 0)}
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+function setup(){
+    grid1.draw();
+    setupGridCordinates();
+}
+setup();
+
+function setupGridCordinates(){
+    for(i = 0; i < lines.length/2; i++){
+        gridX.push(i);
+        gridY.push(i);
+    }
 }
